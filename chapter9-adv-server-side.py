f5aeedc295d00bd0e18059444705cce327d70ddc
Henry-PC <jlf4022163@gmail.com>
2022-03-02 22:06:53 +1100

    cho9 wizard

--- chapter9-adv-server-side.py
+++ chapter9-adv-server-side.py
@@ -10,10 +10,15 @@ book.call_fun()
 create a copy existing record => with ctx => apply function on this copy record
 
 in call_fun() we can set up Condition for record with different ctx
+
+
+#! more about ctx 
+In a record => click button => to a wizard 
+record have ctx => pass to wizard and can be used to set default value for some fields
 # Executing raw SQL queries
-
+#todo
 # Writing a wizard to guide the user
-
+#? step and code in tempalte 
 # Defining onchange methods
 
 # Calling onchange methods on the server side
@@ -23,3 +28,4 @@ in call_fun() we can set up Condition for record with different ctx
 # Adding custom settings options
 
 # Implementing init hooks
+hooks fun 
